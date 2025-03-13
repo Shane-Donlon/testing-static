93ba3f40763479d137bb31986c0af723eedf1862
Shane-Donlon <130906067+Shane-Donlon@users.noreply.github.com>
2025-03-13 10:46:25 +0000

    netlify add

--- astro.config.ts
+++ astro.config.ts
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   integrations: [qwikdev()],
-  adapter: vercel(),
+  adapter: netlify(),
 });
